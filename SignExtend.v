1b613741c84dd3ddbe76b6fc26e23aa4937ab82f
parsaTohidi <45732181+parsaTohidi@users.noreply.github.com>
2018-12-23 16:02:13 +0330

    Update SignExtend.v

--- SignExtend.v
+++ SignExtend.v
@@ -1,13 +1,15 @@
-module sign_extend( in, out );
-input[31:0] in;
-output[63:0] out;
+module signExtend(instruction, extendedAddress);
 
-reg[63:0] out;
-wire[31:0] in;
- base on cb-type and d-type and b-type
-always@(in)
-begin
-    out[31:0] = in[31:0];
-    out[63:32] = {32{in[31]}};
-end
-endmodule 
+	input [31:0] instruction ;
+	output reg [63:0] extended_address;
+	
+	wire opcode = instruction[31:30]; 
+	
+	always @(*) begin
+		case(opcode) 
+   2'b00: extendedAddress = {{38{instruction[25]}} ,instruction[25:0] };    
+			2'b10: extendedAddress = {{45{instruction[23]}} ,instruction[23:5] };
+   2'b11: extendedAddress = {{55{instruction[20]}} ,instruction[20:12] };   //LDUR & STUR
+		endcase
+	end	
+endmodule
